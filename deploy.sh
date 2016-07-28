85ba7eb3249928aa63a03527110b6b515a041f07
Jason <jiatuer@163.com>
2016-07-28 19:06:51 +0800

    rm deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-cd /Users/zhongjian/pentaho/mondrian/
-ant jar
-rm /Users/zhongjian/saiku-kylin/saiku-server2/saiku-server/tomcat/webapps/saiku/WEB-INF/lib/mondrian-4.5.0.2-208.jar
-cp /Users/zhongjian/pentaho/mondrian/dist/mondrian-4.5.0.2-208.jar /Users/zhongjian/saiku-kylin/saiku-server2/saiku-server/tomcat/webapps/saiku/WEB-INF/lib/
-
